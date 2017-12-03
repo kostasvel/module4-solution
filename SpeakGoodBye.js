f57bb4018f3e2bdf72433af44d4fba0fc759e351
kostasvel <33840578+kostasvel@users.noreply.github.com>
2017-12-03 20:50:23 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,5 +1,9 @@
 (function (window) {
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
 
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// See Lecture 52, part 2
 
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
@@ -12,7 +16,7 @@ var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
-
+// See Lecture 52, part 2
 byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
