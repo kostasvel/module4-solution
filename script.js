cbe91536b4a181bc9b98048707ac10a0f9ae6d47
kostasvel <33840578+kostasvel@users.noreply.github.com>
2017-12-03 20:43:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,17 +21,9 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
+
 */
 
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
