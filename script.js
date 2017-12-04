eab380f3d354e9b36eb63c717ef2c86921225b71
kostasvel <33840578+kostasvel@users.noreply.github.com>
2017-12-04 21:41:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "kosta", "tasso", "niko", "laoura", "dimitri", "hanna", "Paula", "paul", " soula"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
